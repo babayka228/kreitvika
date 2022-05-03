c9d2130c38cdd3df4186821467fc13fdbb509f90
babayka228 <98396910+babayka228@users.noreply.github.com>
2022-05-03 15:36:57 +0300

    Add files via upload

---
+++ Python File.py
@@ -0,0 +1,55 @@
+class Microsoft:
+    def __init__(self):
+        self.year=1975
+    def fun(self):
+        print("я появился "+str(self.year)+" года")
+class Win93(Microsoft):
+    def __init__(self):
+        self.year=1993
+class Win95(Microsoft):
+    def __init__(self):
+        self.year=1995
+class Win98(Microsoft):
+    def __init__(self):
+        self.year=1998
+class WinMe(Microsoft):
+    def __init__(self):
+        self.year=2000
+class WinXP(Microsoft):
+    def __init__(self):
+        self.year=2001
+class WinVISTA(Microsoft):
+    def __init__(self):
+        self.year=2007
+class Win7(Microsoft):
+    def __init__(self):
+        self.year=2009
+class Win8(Microsoft):
+    def __init__(self):
+        self.year=2011
+class Win10(Microsoft):
+    def __init__(self):
+        self.year=2015
+class Win11(Microsoft):
+    def __init__(self):
+        self.year=2021
+win93=Win93()
+win95=Win95()
+win98=Win98()
+winMe=WinMe()
+winXP=WinXP()
+winVISTA=WinVISTA()
+win7=Win7()
+win8=Win8()
+win10=Win10()
+win11=Win11()
+win93.fun()
+win95.fun()
+win98.fun()
+winMe.fun()
+winXP.fun()
+winVISTA.fun()
+win7.fun()
+win8.fun()
+win10.fun()
+win11.fun()
